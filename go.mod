78cc9c9b331a5701d3d0f9ccc22275fb5f0c2635
Xie Wei <ampedee@gmail.com>
2023-06-23 15:20:13 +0000

    refactor: use toml library to handle config
    Signed-off-by: Xie Wei <ampedee@gmail.com>

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/openGemini/openGemini-operator
 go 1.19
 
 require (
+	github.com/BurntSushi/toml v1.3.2
 	github.com/onsi/ginkgo/v2 v2.1.4
 	github.com/onsi/gomega v1.19.0
 	github.com/sethvargo/go-password v0.2.0
-	gopkg.in/ini.v1 v1.67.0
 	k8s.io/api v0.25.0
 	k8s.io/apimachinery v0.25.0
 	k8s.io/client-go v0.25.0
--- pkg/configfile/configfile.go
+++ pkg/configfile/configfile.go
@@ -3,58 +3,101 @@ package configfile
 import (
 	"bytes"
 	"fmt"
-	"strings"
 
+	"github.com/BurntSushi/toml"
 	opengeminiv1 "github.com/openGemini/openGemini-operator/api/v1"
 	"github.com/openGemini/openGemini-operator/pkg/naming"
-	"gopkg.in/ini.v1"
 )
+
+type Config struct {
+	Common  CommonConfig  `toml:"common"`
+	Http    HttpConfig    `toml:"http"`
+	Meta    MetaConfig    `toml:"meta"`
+	Data    DataConfig    `toml:"data"`
+	Logging LoggingConfig `toml:"logging"`
+	Gossip  GossipConfig  `toml:"gossip"`
+}
+
+type CommonConfig struct {
+	MetaJoin []string `toml:"meta-join"`
+}
+
+type HttpConfig struct {
+	BindAddress string `toml:"bind-address"`
+}
+
+type MetaConfig struct {
+	BindAddress     string `toml:"bind-address"`
+	HttpBindAddress string `toml:"http-bind-address"`
+	RpcBindAddress  string `toml:"rpc-bind-address"`
+	Dir             string `toml:"dir"`
+	Domain          string `toml:"domain"`
+}
+
+type DataConfig struct {
+	StoreIngestAddr string `toml:"store-ingest-addr"`
+	StoreSelectAddr string `toml:"store-select-addr"`
+	StoreDataDir    string `toml:"store-data-dir"`
+	StoreWalDir     string `toml:"store-wal-dir"`
+	StoreMetaDir    string `toml:"store-meta-dir"`
+}
+
+type LoggingConfig struct {
+	Path string `toml:"path"`
+}
+
+type GossipConfig struct {
+	BindAddress   string   `toml:"bind-address"`
+	StoreBindPort int      `toml:"store-bind-port"`
+	MetaBindPort  int      `toml:"meta-bind-port"`
+	Members       []string `toml:"members"`
+}
 
 func NewConfiguration(cluster *opengeminiv1.GeminiCluster) (string, error) {
 	metaJoinAddrs := []string{}
 	metaGossipAddrs := []string{}
 	for i := 0; i < int(*cluster.Spec.Meta.Replicas); i++ {
 		host := naming.GenerateMetaHeadlessSvc(cluster, i)
-		metaJoinAddrs = append(metaJoinAddrs, fmt.Sprintf(`"%s:8092"`, host))
-		metaGossipAddrs = append(metaGossipAddrs, fmt.Sprintf(`"%s:8010"`, host))
+		metaJoinAddrs = append(metaJoinAddrs, fmt.Sprintf("%s:8092", host))
+		metaGossipAddrs = append(metaGossipAddrs, fmt.Sprintf("%s:8010", host))
 	}
-	metaJoinAddrsStr := "[" + strings.Join(metaJoinAddrs, ", ") + "]"
-	metaGossipAddrsStr := "[" + strings.Join(metaGossipAddrs, ", ") + "]"
 
-	cfg := ini.Empty()
-
-	common, _ := cfg.NewSection("common")
-	common.NewKey("meta-join", metaJoinAddrsStr)
-
-	http, _ := cfg.NewSection("http")
-	http.NewKey("bind-address", `"<HOST_IP>:8086"`)
-
-	meta, _ := cfg.NewSection("meta")
-	meta.NewKey("bind-address", `"<HOST_IP>:8088"`)
-	meta.NewKey("http-bind-address", `"<HOST_IP>:8091"`)
-	meta.NewKey("rpc-bind-address", `"<HOST_IP>:8092"`)
-	meta.NewKey("dir", `"/ogdata/meta"`)
-	meta.NewKey("domain", `"<META_DOMAIN>"`)
-
-	data, _ := cfg.NewSection("data")
-	data.NewKey("store-ingest-addr", `"<HOST_IP>:8400"`)
-	data.NewKey("store-select-addr", `"<HOST_IP>:8401"`)
-	data.NewKey("store-data-dir", `"/ogdata/data"`)
-	data.NewKey("store-wal-dir", `"/ogdata/wal"`)
-	data.NewKey("store-meta-dir", `"/ogdata/meta"`)
-
-	logging, _ := cfg.NewSection("logging")
-	logging.NewKey("path", `"/ogdata/logs"`)
-
-	gossip, _ := cfg.NewSection("gossip")
-	gossip.NewKey("bind-address", `"<HOST_IP>"`)
-	gossip.NewKey("store-bind-port", "8011")
-	gossip.NewKey("meta-bind-port", "8010")
-	gossip.NewKey("members", metaGossipAddrsStr)
+	config := Config{
+		Common: CommonConfig{
+			MetaJoin: metaJoinAddrs,
+		},
+		Http: HttpConfig{
+			BindAddress: "<HOST_IP>:8086",
+		},
+		Meta: MetaConfig{
+			BindAddress:     "<HOST_IP>:8088",
+			HttpBindAddress: "<HOST_IP>:8091",
+			RpcBindAddress:  "<HOST_IP>:8092",
+			Dir:             "/ogdata/meta",
+			Domain:          "<META_DOMAIN>",
+		},
+		Data: DataConfig{
+			StoreIngestAddr: "<HOST_IP>:8400",
+			StoreSelectAddr: "<HOST_IP>:8401",
+			StoreDataDir:    "/ogdata/data",
+			StoreWalDir:     "/ogdata/wal",
+			StoreMetaDir:    "/ogdata/meta",
+		},
+		Logging: LoggingConfig{
+			Path: "/ogdata/logs",
+		},
+		Gossip: GossipConfig{
+			BindAddress:   "<HOST_IP>",
+			StoreBindPort: 8011,
+			MetaBindPort:  8010,
+			Members:       metaGossipAddrs,
+		},
+	}
 
 	buf := new(bytes.Buffer)
-	if _, err := cfg.WriteTo(buf); err != nil {
-		return "", err
+	err := toml.NewEncoder(buf).Encode(config)
+	if err != nil {
+		return "", fmt.Errorf("encode configuration to string failed. err: %w", err)
 	}
 	return buf.String(), nil
 }
